0c18bf1fc5c808818cb39d0652db935bd12845ee
Jaydn Clausen <Jaydnc601@gmail.com>
2015-09-17 10:23:28 -0700

    Madd_libbs complete

--- Mad_libbs.py
+++ Mad_libbs.py
@@ -1,5 +1,12 @@
-name = input ("Please enter a name")
+name = input ("Please enter a name \n")
 MB = name
 MB += " is a normal "
-MB += input ("Enter an occupation")
-MB += " learning how to program at code guild."
+MB += input ("Enter an occupation \n")
+MB += " learning how to program at code guild. Then, one day, Thunder made a new repo called "
+MB += input ("Enter a noun \n")
+MB += " . When {} tried to download the new repo his " .format(name)
+MB += input ("Enter a noun \n") 
+MB += " exploded in the shape of a fiery "
+MB += input ("Enter a shape \n")
+MB += " and {} was never heard from again." .format(name)
+print(MB)
